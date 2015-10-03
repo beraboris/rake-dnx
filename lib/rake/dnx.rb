aaa40e3c55a21e20c9dede4d9817caa27643706f
Boris Bera <bboris@rsoft.ca>
2015-10-03 15:40:39 -0400

    Fixed linter error on file

--- lib/rake/dnx.rb
+++ lib/rake/dnx.rb
@@ -1,6 +1,8 @@
 require 'rake/dnx/version'
 
 module Rake
+  # Rake DNX si a set of helpers that let you discover and run your DNX (.NET
+  # Execution Envrionment) commands from one place easily.
   module Dnx
     # Your code goes here...
   end
