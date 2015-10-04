71a2e0eed7094fc93a667be879afdc9bf7241dcb
Boris Bera <bboris@rsoft.ca>
2015-10-03 23:07:20 -0400

    Added wrapper for the dnx command

--- lib/rake/dnx.rb
+++ lib/rake/dnx.rb
@@ -1,9 +1,47 @@
 require 'rake/dnx/version'
+require 'English'
 
 module Rake
   # Rake DNX si a set of helpers that let you discover and run your DNX (.NET
   # Execution Envrionment) commands from one place easily.
   module Dnx
-    # Your code goes here...
+    # Represents a failure when running the `dnx` command
+    class DnxError < RuntimeError
+      attr_reader :exit_code, :command, :params
+
+      def initialize(exit_code, command, params = {})
+        @exit_code = exit_code
+        @command = command
+        @params = params
+
+        super "Failed to run 'dnx #{command}' with param=#{params} ' \
+          '(exit_code=#{exit_code})"
+      end
+    end
+
+    # Represents that the dnx command doesn't exist
+    class DnxNotFoundError < RuntimeError
+      def initialize
+        super 'Failed to find the dnx command. Make sure it is installed'
+      end
+    end
+
+    # Calls the `dnx` command
+    #
+    # Ignoring :reek:NilCheck since it's a part of `system` and has a special
+    #   meaning
+    def dnx(command, project: nil)
+      options = []
+      options += ['--project', project] if project
+      res = system 'dnx', *options, command
+
+      case res
+      when false
+        fail DnxError.new($CHILD_STATUS, command)
+      when nil
+        fail DnxNotFoundError
+      end
+    end
+    module_function :dnx
   end
 end
--- spec/rake/dnx_spec.rb
+++ spec/rake/dnx_spec.rb
@@ -1,7 +1,37 @@
 require 'spec_helper'
 
 describe Rake::Dnx do
-  it 'has a version number' do
+  it 'should have a version number' do
     expect(Rake::Dnx::VERSION).not_to be nil
+  end
+
+  describe '::dnx' do
+    it 'should call the dnx command with the given subcommand' do
+      expect(Rake::Dnx).to receive(:system)
+        .with('dnx', 'some-subcommand')
+        .and_return true
+
+      Rake::Dnx.dnx 'some-subcommand'
+    end
+
+    it 'should fail if the dnx command fails' do
+      allow(Rake::Dnx).to receive(:system).and_return false
+
+      expect { Rake::Dnx.dnx('something') }.to raise_error(Rake::Dnx::DnxError)
+    end
+
+    it 'should fail if the dnx command is missing' do
+      allow(Rake::Dnx).to receive(:system).and_return nil
+
+      expect { Rake::Dnx.dnx('something') }
+        .to raise_error(Rake::Dnx::DnxNotFoundError)
+    end
+
+    it 'should pass the project argument' do
+      expect(Rake::Dnx).to receive(:system)
+        .with('dnx', '--project', 'My.Super.Project', 'thing').and_return true
+
+      Rake::Dnx.dnx 'thing', project: 'My.Super.Project'
+    end
   end
 end
