cb9ee0e945dfcf15074572ee8184ae72a4ef8c37
Boris Bera <bboris@rsoft.ca>
2015-10-08 20:34:21 -0400

    Testing that helpers are injected into the main object

--- spec/rake/dnx_spec.rb
+++ spec/rake/dnx_spec.rb
@@ -1,5 +1,13 @@
+MAIN_OBJECT = self
+
 describe Rake::Dnx do
   it 'should have a version number' do
     expect(Rake::Dnx::VERSION).not_to be nil
+  end
+
+  it 'should inject itself into the main object' do
+    expect(MAIN_OBJECT).to respond_to :dnx
+    expect(MAIN_OBJECT).to respond_to :dnu
+    expect(MAIN_OBJECT).to respond_to :dnx_discover
   end
 end
