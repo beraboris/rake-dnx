920649c0c43179b4debf3c918c841012703b314f
Boris Bera <bboris@rsoft.ca>
2015-10-12 01:04:30 -0400

    Finished implementing Word and WordTest

--- examples/enterprise-hello-world/src/Acme.HelloWorld.Data/Word.cs
+++ examples/enterprise-hello-world/src/Acme.HelloWorld.Data/Word.cs
@@ -1,19 +1,39 @@
 using System;
+using System.Text.RegularExpressions;
 
 namespace Acme.HelloWord.Data {
     public class Word {
         public string Value { get; }
         public Language Language { get; }
 
-        public Word(string val) {
-            if (val.Contains(" ")) throw new ArgumentException("val");
+        public Word(string val): this(val, Language.English) { }
+
+        public Word(string val, Language language) {
+            if (new Regex(@"\s").IsMatch(val))
+                throw new ArgumentException("Word can't contain a witespace character", "val");
 
             Value = val;
-            Language = Language.English;
+            Language = language;
         }
 
         public override string ToString() {
             return Value;
+        }
+
+        public override bool Equals(object other) {
+            if (other == null) return false;
+            var otherWord = other as Word;
+            if (otherWord == null) return false;
+
+            return Value.Equals(otherWord.Value)
+                && this.Language.Equals(otherWord.Language);
+        }
+
+        public override int GetHashCode() {
+            var hash = 17;
+            hash = hash * 23 + Value.GetHashCode();
+            hash = hash * 23 + Language.GetHashCode();
+            return hash;
         }
     }
 }
--- examples/enterprise-hello-world/test/Acme.HelloWorld.Data.Test/WordTest.cs
+++ examples/enterprise-hello-world/test/Acme.HelloWorld.Data.Test/WordTest.cs
@@ -3,9 +3,34 @@ using System;
 
 namespace Acme.HelloWord.Data {
     public class WordTest {
+        [Theory]
+        [InlineData("foo bar")]
+        [InlineData("foo\tbar")]
+        [InlineData("foo\nbar")]
+        [InlineData("foo\rbar")]
+        public void ConstructorShouldBlowupWhenWordHasSpaces(string val) {
+            Assert.Throws<ArgumentException>(() => new Word(val));
+        }
+
         [Fact]
-        public void ConstructorShouldBlowupWhenWordHasSpaces() {
-            Assert.Throws<ArgumentException>(() => new Word("foo bar"));
+        public void OneArgConstructorShouldDefaultToEnglish() {
+            Assert.Equal(Language.English, new Word("something").Language);
+        }
+
+        [Fact]
+        public void ToStringShouldReturnValue() {
+            Assert.Equal("Stuff", new Word("Stuff").ToString());
+        }
+
+        [Fact]
+        public void EqualsShouldMatchOnValueAndLanguage() {
+            Assert.Equal(new Word("Foobar"), new Word("Foobar"));
+        }
+
+        [Fact]
+        public void GetHashCodeShouldMatchForSameObject() {
+            Assert.Equal(new Word("Thing").GetHashCode(),
+                new Word("Thing").GetHashCode());
         }
     }
 }
